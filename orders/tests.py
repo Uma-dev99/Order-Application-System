4839444a0f7484fa3e37b9dcbf6e7a1b745e8ebb
uma-dev99 <umashakar.singh24101999@ug.cusat.ac.in>
2024-10-15 17:58:40 +0530

    Updated tests.py

--- orders/tests.py
+++ orders/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import Orders
 from .forms import OrderForm
-
 class OrdersModelTests(TestCase):
 
     def test_orders_creation(self):
@@ -11,7 +10,7 @@ class OrdersModelTests(TestCase):
             fname='Uma Shankar',
             lname='Singh',
             price=235,
-            mail='john@example.com',
+            mail='umash@gmail.com',
             addr='Sector 49'
         )
         self.assertEqual(order.fname, 'Uma Shankar')
@@ -34,10 +33,10 @@ class OrdersViewsTests(TestCase):
 
         self.order = Orders.objects.create(
             oid=1,
-            fname='John',
-            lname='Doe',
+            fname='Abhishek',
+            lname='Singh',
             price=99.99,
-            mail='john@example.com',
+            mail='abhiakauma@gmail.com',
             addr='123 Main St'
         )
 
@@ -51,7 +50,7 @@ class OrdersViewsTests(TestCase):
         response = self.client.get(reverse('show_url'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'orders/show.html')
-        self.assertContains(response, 'John')
+        self.assertContains(response, 'Abhishek')
     
     def test_update_view_get(self):
         response = self.client.get(reverse('update_url', args=[self.order.oid]))
@@ -62,21 +61,10 @@ class OrdersViewsTests(TestCase):
     def test_update_view_post_valid(self):
         response = self.client.post(reverse('update_url', args=[self.order.oid]), {
             'oid': 1,
-            'fname': 'Updated Name',
-            'lname': 'Doe',
+            'fname': 'Abhishek',
+            'lname': 'Roy',
             'price': 89.99,
-            'mail': 'updated@example.com',
-            'addr': '789 Oak St'
+            'mail': 'aroy@example.com',
+            'addr': 'Samstipur'
         })
-        self.assertRedirects(response, reverse('show_url'))    
-    
-    def test_delete_view_get(self):
-        response = self.client.get(reverse('delete_url', args=[self.order.oid]))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'orders/confirmation.html')
-        self.assertContains(response, 'Uma Shankar')
-
-    def test_delete_view_post(self):
-        response = self.client.post(reverse('delete_url', args=[self.order.oid]))
         self.assertRedirects(response, reverse('show_url'))
-        self.assertEqual(Orders.objects.count(), 0)
